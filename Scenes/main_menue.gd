e08774202a61498ffdcf725d82ecda3252f1f21e
NoobieBubie <bas.tolga@web.de>
2025-10-30 22:11:13 +0100

    Scheis Transition scheiße 🐛
    Wollte diesen Müll mit den Transitions machen die Bea unbedingt wilt. >.>
    wer braucht schon schöne übergänge...
    man ich hab nicht mal in meinern PowerPoints übergänge rein gemacht einfach knall hart rein da mit Anlauf.
    Find auch diese Müll einstellung die mir die anleitung sagt nicht was das für kack meine IDE ist teils auf Deutsch teils auf Englisch wer hat sich den Müll ausgedacht mach halt gleich nur englisch.

--- Scenes/main_menue.gd
+++ Scenes/main_menue.gd
@@ -3,8 +3,13 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+# Sage unserem globalen Fader, er soll "einfaden"
+	# (Wir gehen davon aus, dass der Bildschirm schwarz ist)
+	# Um sicherzugehen, dass es beim allerersten Start klappt,
+	# setzen wir den Fader erst auf "schwarz" und starten dann fade_in.
+	#SceneTransition.animation_player.play("fade_out") # Setzt auf schwarz (ohne await)
+	#SceneTransition.animation_player.play("fade_in")  # Startet den Fade-In
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
